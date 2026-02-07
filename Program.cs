6f85d691564c764264b2acefa001b304fe81af0d
Rajveer Singh <108996297+subbhamsingh@users.noreply.github.com>
2026-02-07 23:22:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day1_CSharp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name  + "Welcome to Day 1st of learning of C#");
+
+            Console.WriteLine("Enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("sum = " + (a + b));
+            Console.WriteLine("Difference = " + (a - b));
+            Console.WriteLine("Product = " + (a * b));
+
+        }
+    }
+}

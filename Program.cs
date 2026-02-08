9030af226d7a736eab62ea2b398ef144b10cb546
Rajveer Singh <108996297+subbhamsingh@users.noreply.github.com>
2026-02-08 21:35:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Day1_CSharp
+class  Program
 {
-     class Program
+    static List<string> books = new List<string>();
+     static void Main()
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Enter your name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name  + "Welcome to Day 1st of learning of C#");
+            Console.WriteLine("Book Manager System");
+            Console.WriteLine("1. Add a Book");
+            Console.WriteLine("2. Remove a Book");
+            Console.WriteLine("3. Search a Book");
+            Console.WriteLine("4.Exit from system");
 
-            Console.WriteLine("Enter first number");
-            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Choose the option from menu");
+            int choice=Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter second number");
-            int b = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: AddBook();
+                    break;
+                case 2: RemoveBook();
+                    break;
+                case 3: SearchBook();
+                    break;
+                case 4: return;
+                default: Console.WriteLine("Invalid choice");
+                    break;
+            }
 
-            Console.WriteLine("sum = " + (a + b));
-            Console.WriteLine("Difference = " + (a - b));
-            Console.WriteLine("Product = " + (a * b));
+        }
+    }
 
+    static void AddBook()
+    {
+        Console.WriteLine("Enter Book name");
+        string book= Console.ReadLine(); 
+        if(!string.IsNullOrEmpty(book))
+        {
+            books.Add(book);
+            Console.WriteLine("Book Added");
+        }
+        else
+        {
+            Console.WriteLine("Invalid name");
+        }
+    }
+
+    static void RemoveBook()
+    {
+        Console.Write("Enter book name to remove");
+        string book=Console.ReadLine();
+        if (books.Remove(book))
+        {
+            Console.WriteLine("Book removed");
+
+        }
+        else
+        {
+            Console.WriteLine("Book not found");
+        }
+    }
+
+    static void SearchBook()
+    {
+        Console.Write("Enter book name to search");
+        string book = Console.ReadLine();
+        if (books.Contains(book))
+        {
+            Console.WriteLine("Book found");
+
+        }
+        else
+        {
+            Console.WriteLine("Book not found");
         }
     }
 }
